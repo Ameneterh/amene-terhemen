a2fd91b9546a3669478bcbadc34f35334485d616
Amene Terhemen <ameneterh@gmail.com>
2024-01-21 22:02:30 +0100

    create user signup api

--- api/index.js
+++ api/index.js
@@ -3,16 +3,21 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
+import authRouter from "./routes/auth.route.js";
+
 mongoose
   .connect(process.env.MONGO_DB)
   .then(() => {
-    console.log("COnntected to MongoDB!");
+    console.log("Connected to MongoDB!");
   })
   .catch((err) => {
     console.log(err);
   });
 const app = express();
+app.use(express.json());
 
 app.listen(5050, () => {
   console.log("Server is running on Port 5050");
 });
+
+app.use("/api/auth", authRouter);
