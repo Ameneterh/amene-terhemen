d04efd358b02e78ea6faa9aa007e25a20ba35b2a
Amene Terhemen <ameneterh@gmail.com>
2024-01-26 12:33:34 +0100

    complete signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,18 +1,75 @@
 import { Button, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/login");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
-        <TextInput type="text" placeholder="username" id="username" />
-        <TextInput type="email" placeholder="email" id="email" />
-        <TextInput type="password" placeholder="password" id="password" />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <TextInput
+          type="text"
+          placeholder="username"
+          id="username"
+          onChange={handleChange}
+        />
+        <TextInput
+          type="email"
+          placeholder="email"
+          id="email"
+          onChange={handleChange}
+        />
+        <TextInput
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+        />
 
-        <Button outline gradientDuoTone="purpleToBlue">
-          Sign Up
+        <Button
+          disabled={loading}
+          outline
+          gradientDuoTone="purpleToBlue"
+          type="submit"
+        >
+          {loading ? "Loading ..." : "Sign Up"}
         </Button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -21,6 +78,7 @@ export default function SignUp() {
           <span className="text-blue-700 hover:underline">Login</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
