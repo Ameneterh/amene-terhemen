5e4079d90bf105c231cf532068dbf1173628274e
Amene Terhemen <ameneterh@gmail.com>
2024-01-26 13:28:22 +0100

    complete login page functionality

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,78 @@
-import React from "react";
+import { Button, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Login</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <TextInput
+          type="email"
+          placeholder="email"
+          id="email"
+          onChange={handleChange}
+        />
+        <TextInput
+          type="password"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+        />
+
+        <Button
+          disabled={loading}
+          outline
+          gradientDuoTone="purpleToBlue"
+          type="submit"
+        >
+          {loading ? "Logging in ..." : "Login"}
+        </Button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Do not have an account?</p>
+        <Link to="/signup">
+          <span className="text-blue-700 hover:underline">Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
