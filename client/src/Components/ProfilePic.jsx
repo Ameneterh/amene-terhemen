2a7756ec851574e5fa9a0e51e970202a5810b507
Amene Terhemen <ameneterh@gmail.com>
2024-02-03 20:34:49 +0100

    add alt to profile img

--- client/src/Components/ProfilePic.jsx
+++ client/src/Components/ProfilePic.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default function ProfilePic() {
   return (
     <div className="fixed">
-      <img src="/profilepic.png/" className=" h-screen object-cover" />
+      <img
+        src="/profilepic.png/"
+        alt="amene terhemen profile image"
+        className=" h-screen object-cover"
+      />
     </div>
   );
 }
